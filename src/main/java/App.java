93750b444022b57e913478d68c187aada4e77152
danielogen <danielogen@gmail.com>
2023-09-28 18:56:28 -0700

    Testing-Rename Class Refactoring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,11 @@
-import controller.Foo;
-
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
 
         // Testing controller.Foo
-        Foo foo = new Foo();
-        foo.displayInnerClasses();
+//        Foo foo = new Foo();
+//        foo.displayInnerClasses();
 
         //display refactoring
         RefactorTest refactorTest = new RefactorTest();
