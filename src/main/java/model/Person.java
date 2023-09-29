bad8c5cc467964e1e193a8a9efc6bb4466031d83
danielogen <danielogen@gmail.com>
2023-09-29 00:18:46 -0700

    add person class

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Person extends Mammal {
+    private String name;
+    private int phoneHome;
+    private int phoneOffice;
+    private int fax;
+    private String address;
+
+
+    public String getAddress(){
+        return this.address;
+    }
+}
